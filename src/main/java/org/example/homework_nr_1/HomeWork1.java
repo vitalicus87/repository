9d4a01d8fc41f868e2f80b4234e3d516027172c4
vitalicus87 <vitalicus87@gmail.com>
2023-03-21 20:58:33 +0200

    Homework 1

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -0,0 +1,28 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {System.out.println("Hello world!");
+
+   long CurrentWorldPopulation = 7811080815L;
+   int CarsProducedThisYear = 55235358;
+   char FirstLetterInAlphabet = 'A';
+   boolean JavaIsAmazing = true;
+   float Pi = 3.14159F;
+
+   System.out.println(CurrentWorldPopulation);
+   System.out.println(CarsProducedThisYear);
+   System.out.println(FirstLetterInAlphabet);
+   System.out.println(JavaIsAmazing);
+   System.out.println(Pi);
+
+   double price = 40.4020;
+   double tax = 2.45;
+   int Quantity = 5448;
+   double sum = 0;
+
+   double total = price + tax * Quantity + sum;
+   System.out.println(total);
+
+
+    }
+}
